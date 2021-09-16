f5fbffa365085e3dc61f1302cabb9dae2054ebbf
mjingram <micha.j.ingram@gmail.com>
2021-09-16 16:34:00 -0500

    insert Node at Tail method and remove Node method

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -21,6 +21,14 @@ public class SinglyLinkedList {
 		}
 	}
 	
+	public Node deleteNode(Node node, int position) {
+		if(position == 0) {
+			return node.getNext();
+		}
+		node.setNext(deleteNode(node.getNext(), position-1));
+		return node;
+	}
+	
 	public void display() {
 		Node current = head;
 		
@@ -36,4 +44,16 @@ public class SinglyLinkedList {
 		}
 		System.out.println(tail.getData());
 	}
+	
+	public Node insertNodeAtTail(Node node, int data) {
+        if(node == null){
+            return new Node(data);
+        }
+        else{
+            node.setNext(insertNodeAtTail(node.getNext(), data));
+        }
+        return node;    
+        
+
+    }
 }
